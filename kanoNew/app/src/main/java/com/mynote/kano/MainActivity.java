d49f8c2d6d66899e100434bffef2f231b3c6f996
JEONGJIYOUN <jjy134347@gmail.com>
2020-08-21 22:38:13 +0900

    test

--- kanoNew/app/src/main/java/com/mynote/kano/MainActivity.java
+++ kanoNew/app/src/main/java/com/mynote/kano/MainActivity.java
@@ -1,11 +1,9 @@
 package com.mynote.kano;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goLogin(View v){
+
     }
 
 }
