2306384b081d87b1622c08c29988396cb08c8324
JEONGJIYOUN <jjy134347@gmail.com>
2020-08-22 01:48:56 +0900

    loginEnd

--- kanoNew/app/src/main/java/com/mynote/kano/MainActivity.java
+++ kanoNew/app/src/main/java/com/mynote/kano/MainActivity.java
@@ -5,16 +5,28 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.mynote.kano.model.LoginModel;
+
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
     }
 
     public void goLogin(View v){
-
+        LoginModel loginModel = new LoginModel();
+        loginModel.doLogin(this);
     }
-
 }
