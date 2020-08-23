a4dee467c21a45f4ee86f86aefe40fa13b610da7
JEONGJIYOUN <jjy134347@gmail.com>
2020-08-23 21:07:01 +0900

    login And commit Data

--- kanoNew/app/src/main/java/com/mynote/kano/MainActivity.java
+++ kanoNew/app/src/main/java/com/mynote/kano/MainActivity.java
@@ -1,32 +1,20 @@
 package com.mynote.kano;
 
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.mynote.kano.model.LoginModel;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
-    FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-
-    }
-
-    public void goLogin(View v){
-        LoginModel loginModel = new LoginModel();
-        loginModel.doLogin(this);
-    }
 }
